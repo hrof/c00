2be1144c9d1b738856c2b804b8e17ced3c7edb6e
Roman Zhdanov <rzhdanov@car5s5.42barcelona.com>
2022-11-24 19:25:07 +0100

    applied the norm and commented out main function in ex04

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzhdanov <rzhdanov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 18:41:24 by rzhdanov          #+#    #+#             */
+/*   Updated: 2022/11/24 19:24:19 by rzhdanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_is_negative(int n);
+void	ft_is_negative(int n);
 
-void ft_is_negative(int n)
+void	ft_is_negative(int n)
 {
 	char	sign;
+
 	sign = 80;
 	if (n < 0)
 	{
@@ -12,9 +25,11 @@ void ft_is_negative(int n)
 	}
 	write(1, &sign, 1);
 }
-
-int main(void)
+/*
+int	main(void)
 {
 	ft_is_negative(4);
 	ft_is_negative(-10);
+	ft_is_negative(0);
 }
+*/

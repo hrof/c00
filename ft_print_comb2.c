71ae8bbee0cf34091b3f4910cea8dae68500cd38
Roman Zhdanov <rzhdanov@car5s5.42barcelona.com>
2022-11-24 21:47:53 +0100

    i have a feeling that I should first figure out ex07, cause operating on 4 independent figures seems too difficult to do

---
+++ ft_print_comb2.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzhdanov <rzhdanov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 21:21:28 by rzhdanov          #+#    #+#             */
+/*   Updated: 2022/11/24 21:46:30 by rzhdanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_mul_char(char d1, char u1, char d2, char u2);
+
+void	ft_print_mul_char(char d1, char u1, char d2, char u2)
+{
+	ft_putchar(d1);
+	ft_putchar(u1);
+	wite(1, ' ', 1);
+	ft_putchar(d2);
+	ft_putchar(u2);
+	if (d1 != 9 && u1 != 8)
+	{
+		write(1, ", ", 2);
+	}
+}
+
+int	main(void)
+{
+	char	d1;
+	char	u1;
+	char	d2;
+	char	u2;
+
+	d1 = '0';
+	u1 = '0';
+
+	while (d1 != 9 && u1 != 8)
+	{
+		u2 = u1 + 1;
+		d2 = 0;
+		while (d2 <= '9')
+		{
+			u2 = d2;
+			while (u2 <= '9')
+			{
+				u2 ++;
+			}
+			d2 ++
+		}
+	}
+}

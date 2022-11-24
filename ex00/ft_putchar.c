03055caab43e480e4bda740fc6dce374232a8d62
Roman Zhdanov <rzhdanov@cbr6s3.42barcelona.com>
2022-11-24 14:54:20 +0100

    norminette applied to ex01

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -1,15 +1,29 @@
-#include <unistd.h>
-void ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzhdanov <rzhdanov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 14:49:15 by rzhdanov          #+#    #+#             */
+/*   Updated: 2022/11/24 14:53:27 by rzhdanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putchar(char c)
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putchar(char c)
 {
 	write(1, &c, 2);
 }
 
-int main(void)
+int	main(void)
 {
-	char c = 'a';
-	ft_putchar(c);
-	return(0);
-}
+	char	c;
 
+	c = 'a';
+	ft_putchar(c);
+	return (0);
+}

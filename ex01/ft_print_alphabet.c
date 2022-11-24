018fae812fa246f768162a25becf175f0ffeb04c
Roman Zhdanov <rzhdanov@cbr6s3.42barcelona.com>
2022-11-24 11:50:39 +0100

    ex02 complete

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,16 @@
+
+#include <unistd.h>
+void ft_print_alphabet(void);
+
+void ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+
+int main(void)
+{
+	ft_print_alphabet();
+}
+
+
+

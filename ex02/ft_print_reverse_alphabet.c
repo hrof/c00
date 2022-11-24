e10ac9536bbc5b502180593d39d8e68c2eb593cd
Roman Zhdanov <rzhdanov@car5s5.42barcelona.com>
2022-11-24 18:38:05 +0100

    applied norminette and commented out main in ex02

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -6,26 +6,28 @@
 /*   By: rzhdanov <rzhdanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:50:40 by rzhdanov          #+#    #+#             */
-/*   Updated: 2022/11/24 13:57:21 by rzhdanov         ###   ########.fr       */
+/*   Updated: 2022/11/24 18:37:26 by rzhdanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-void ft_print_reverse_alphabet(void);
 
-void ft_print_reverse_alphabet(void)
+void	ft_print_reverse_alphabet(void);
+
+void	ft_print_reverse_alphabet(void)
 {
 	char	c;
+
 	c = 122;
-	while(c >= 97)
+	while (c >= 97)
 	{	
-		write(1, &c, 1);
+		write (1, &c, 1);
 		c --;
 	}
 }
-
+/*
 int	main(void)
 {
 	ft_print_reverse_alphabet();
 }
-
+*/

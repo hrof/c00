3b5e0109c705ac4c992d04f841072b93ecab1f8a
Roman Zhdanov <rzhdanov@cbr6s3.42barcelona.com>
2022-11-24 15:54:00 +0100

    ran norminette on ex01 and commented out main

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -1,16 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzhdanov <rzhdanov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 15:28:10 by rzhdanov          #+#    #+#             */
+/*   Updated: 2022/11/24 15:49:20 by rzhdanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
-void ft_print_alphabet(void);
 
-void ft_print_alphabet(void)
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
 {
 	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 }
-
-int main(void)
+/*
+int	main(void)
 {
 	ft_print_alphabet();
 }
-
-
-
+*/

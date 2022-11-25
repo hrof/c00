61615045a7af4df48e7d41c4307764f57222ad41
Roman Zhdanov <rzhdanov@cbr8s5.42barcelona.com>
2022-11-25 12:12:49 +0100

    added .gitignore and sample of printf working (to be deleted)

---
+++ ex06/printfexample.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main () {
+   int ch;
+
+   for( ch = 75 ; ch <= 100; ch++ ) {
+      printf("ASCII value = %d, Character = %c\n", ch , ch );
+   }
+
+   return(0);
+}

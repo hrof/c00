4e5c7a8052dc4bcf219041fe369803d244e0395e
Roman Zhdanov <rzhdanov@cbr8s5.42barcelona.com>
2022-11-25 12:15:17 +0100

    printfexample reduced to printing one line

--- ex06/printfexample.c
+++ ex06/printfexample.c
@@ -4,9 +4,9 @@
 int main () {
    int ch;
 
-   for( ch = 75 ; ch <= 100; ch++ ) {
-      printf("ASCII value = %d, Character = %c\n", ch , ch );
-   }
+   ch = 76;
+
+   printf("ASCII value = %d, Character = %c\n", ch , ch );
 
    return(0);
 }
